a4a7c027fc4f7cf888b392cac10612345ba8085a
kbini28 <kbini28@gmail.com>
2020-08-07 15:38:00 -0400

    Event index shows on the browser
    Index is working and will show events on the browser.
    
    Shows all events, not just owner's events
    Will save that until after all CRUD actions are completed.

---
+++ src/components/WellnessEventIndex/WellnessEventIndex.js
@@ -0,0 +1,92 @@
+import React, { useState, useEffect } from 'react'
+// import { Link } from 'react-router-dom'
+import axios from 'axios'
+import apiUrl from '../../apiConfig'
+import messages from '../AutoDismissAlert/messages'
+
+const WellnessEventIndex = props => {
+  const [wellnessEvents, setWellnessEvents] = useState([])
+
+  useEffect(() => {
+    const { user, msgAlert } = props
+    axios({
+      method: 'GET',
+      url: `${apiUrl}/wellnessEvents`,
+      headers: {
+        'Authorization': `Token token=${user.token}`
+      }
+    })
+      .then(response => {
+        console.log(response.data)
+        setWellnessEvents({
+          wellnessEvents: response.data.wellnessEvents
+        })
+      })
+      .then(() => msgAlert({
+        heading: 'Event Show Success',
+        message: messages.wellnessIndexSuccess,
+        variant: 'Success'
+      }))
+      .catch(console.error)
+  }, [])
+
+  let jsx
+  // if the API has not responded yet
+  if (wellnessEvents === null) {
+    jsx = <p>Loading...</p>
+
+  // if the API responds with no events
+  } else if (wellnessEvents.length === 0) {
+    jsx = <p>No wellness events created, yet. Please add an event!</p>
+  // if the API responds with events
+  } else if (wellnessEvents.wellnessEvents.location === '') {
+    console.log('here is where I log wellnessEvents', wellnessEvents)
+    jsx = (
+      <div className="row">
+        <div className="col-sm-10 col-md-8 mx-auto mt-5 wellness-event-index">
+          <ul>
+            {wellnessEvents.wellnessEvents.map((wellnessEvent) => {
+              return (
+                <li key={wellnessEvent._id}>
+                  <h3>Practicing wellness with: {wellnessEvent.eventType}</h3>
+                  <h4>When? {wellnessEvent.date}</h4>
+                  <h4>Start: {wellnessEvent.startTime}</h4>
+                  <h4>End: {wellnessEvent.endTime}</h4>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  } else {
+    jsx = (
+      <div className="row">
+        <div className="col-sm-10 col-md-8 mx-auto mt-5 wellness-event-index">
+          <ul>
+            {wellnessEvents.wellnessEvents.map((wellnessEvent) => {
+              return (
+                <li key={wellnessEvent._id}>
+                  <h3>Practicing wellness with: {wellnessEvent.eventType}</h3>
+                  <h4>When? {wellnessEvent.date}</h4>
+                  <h4>Start: {wellnessEvent.startTime}</h4>
+                  <h4>End: {wellnessEvent.endTime}</h4>
+                  <h4>Where? {wellnessEvent.location}</h4>
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+
+  return (
+    <div className="wellness-event-index">
+      <h2>All Wellness Events:</h2>
+      {jsx}
+    </div>
+  )
+}
+
+export default WellnessEventIndex
