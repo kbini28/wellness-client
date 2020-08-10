5996133a4cca8c7814a23fed1db4c87e813dd050
kbini28 <kbini28@gmail.com>
2020-08-10 16:33:44 -0400

    Not sure why it's making me commit this
    No changes made.

---
+++ src/components/Home/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Home = () => (
+  <main>
+    <h1>Welcome to Back in 10!</h1>
+    <h2>Take a minute (or 10) to breathe, unplug, and focus on your health!</h2>
+  </main>
+)
+
+export default Home
